4af8cd07bfef4e7f50445543257b25bf8b3693a2
Andrzej Wójcik <andrzejw@tigase.org>
2015-01-21 17:40:55 +0100

    Issue #2615 - patched PostgreSQL SQL table creation query

--- src/main/java/tigase/archive/db/JDBCMessageArchiveRepository.java
+++ src/main/java/tigase/archive/db/JDBCMessageArchiveRepository.java
@@ -277,6 +277,7 @@ public class JDBCMessageArchiveRepository extends AbstractMessageArchiveReposito
 																									MSGS_TYPE + " varchar(10), " +
 																									MSGS_BODY + " text, " +
 																									MSGS_MSG + " text," +
+																									" primary key (" + MSGS_ID + ")," +
 																									" foreign key (" + MSGS_BUDDY_ID +
 																									") references " + JIDS_TABLE + " (" +
 																									JIDS_ID + ")," + " foreign key (" +
