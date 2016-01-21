53dc8c235780666f76fce1c5c853189741242d1d
Andrzej Wójcik <andrzejw@tigase.org>
2016-01-21 15:41:15 +0100

    Issue #3543 - updated licence information in schema files headers

--- database/sqlserver-schema.sql
+++ database/sqlserver-schema.sql
@@ -1,12 +1,19 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  andrzej
- * Created: 2016-01-18
- */
+ --
+--  Tigase Message Archiving Component
+--  Copyright (C) 2016 "Tigase, Inc." <office@tigase.com>
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU Affero General Public License as published by
+--  the Free Software Foundation, either version 3 of the License.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Affero General Public License for more details.
+--
+--  You should have received a copy of the GNU Affero General Public License
+--  along with this program. Look for COPYING file in the top folder.
+--  If not, see http://www.gnu.org/licenses/.
 
 -- QUERY START:
 SET QUOTED_IDENTIFIER ON
