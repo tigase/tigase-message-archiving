176e1812da216e4117618f89acf0286cd53b3e40
Andrzej Wójcik <andrzejw@tigase.org>
2016-08-03 19:58:39 +0200

    Task #4375 - added support for custom query fields (contains, tags)

--- src/main/java/tigase/archive/xep0313/MAMQueryParser.java
+++ src/main/java/tigase/archive/xep0313/MAMQueryParser.java
@@ -21,7 +21,15 @@
  */
 package tigase.archive.xep0313;
 
+import tigase.archive.MessageArchiveConfig;
+import tigase.component.exceptions.ComponentException;
 import tigase.kernel.beans.Bean;
+import tigase.kernel.beans.Inject;
+import tigase.server.DataForm;
+import tigase.server.Packet;
+import tigase.xml.Element;
+import tigase.xmpp.Authorization;
+import tigase.xmpp.mam.Query;
 
 /**
  * Created by andrzej on 19.07.2016.
@@ -29,4 +37,53 @@ import tigase.kernel.beans.Bean;
 @Bean(name = "mamQueryParser")
 public class MAMQueryParser extends tigase.xmpp.mam.MAMQueryParser {
 
+	private static final String CONTAINS_FIELD_NAME = "tigase:body:contains";
+	private static final String TAGS_FIELD_NAME = "tigase:tags";
+
+	@Inject
+	private MessageArchiveConfig config;
+
+	@Override
+	public Query parseQuery(Query query, Packet packet) throws ComponentException {
+		Query result = super.parseQuery(query, packet);
+
+		Element queryEl = packet.getElement().getChildStaticStr("query", MAM_XMLNS);
+		String[] contains = DataForm.getFieldValues(queryEl, CONTAINS_FIELD_NAME);
+		if (contains != null && contains.length > 0) {
+			if (!(query instanceof tigase.archive.xep0136.Query))
+				throw new ComponentException(Authorization.BAD_REQUEST, "Unsupported feature " + CONTAINS_FIELD_NAME);
+
+			for (String it : contains) {
+				((tigase.archive.xep0136.Query) query).addContains(it);
+			}
+		}
+
+		String[] tags = DataForm.getFieldValues(queryEl, TAGS_FIELD_NAME);
+		if (tags != null && tags.length > 0) {
+			if (!(query instanceof tigase.archive.xep0136.Query) || !config.isTagSupportEnabled())
+				throw new ComponentException(Authorization.BAD_REQUEST, "Unsupported feature " + TAGS_FIELD_NAME);
+
+			for (String it : tags) {
+				((tigase.archive.xep0136.Query) query).addTag(it);
+			}
+		}
+
+		return result;
+	}
+
+	@Override
+	public Element prepareForm(Element elem) {
+		Element form = super.prepareForm(elem);
+		Element x = form.getChild("x", "jabber:x:data");
+
+		if (x != null) {
+			addField(x, CONTAINS_FIELD_NAME, "text-multi", "Contains in body");
+
+			if (config.isTagSupportEnabled()) {
+				addField(x, TAGS_FIELD_NAME, "text-multi", "Contains tags");
+			}
+		}
+
+		return form;
+	}
 }
