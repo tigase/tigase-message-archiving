bd291d513846571ddfcc5608387608ec409d59e3
Andrzej Wójcik <andrzejw@tigase.org>
2017-06-24 11:26:16 +0200

    #5711: fixing compilation error after recent changes in Tigase XMPP Server API

--- src/test/java/tigase/archive/db/AbstractMessageArchiveRepositoryTest.java
+++ src/test/java/tigase/archive/db/AbstractMessageArchiveRepositoryTest.java
@@ -93,7 +93,7 @@ public abstract class AbstractMessageArchiveRepositoryTest {
 	}
 
 	@Before
-	public void setup() throws DBInitException, InstantiationException, IllegalAccessException, SQLException, ClassNotFoundException {
+	public void setup() throws RepositoryException, InstantiationException, IllegalAccessException, SQLException, ClassNotFoundException {
 		if (uri == null)
 			return;
 
